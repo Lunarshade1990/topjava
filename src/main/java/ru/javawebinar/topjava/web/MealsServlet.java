6880bad809397f63776257ef5d9c36fea6f314d5
LunarShade <na.yada1990@gmail.com>
2023-02-06 13:03:27 +0300

    added get logging

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,12 +13,14 @@ import java.time.LocalTime;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
-import static ru.javawebinar.topjava.util.MealsUtil.*;
+import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
+import static ru.javawebinar.topjava.util.MealsUtil.getMeals;
 
 public class MealsServlet extends HttpServlet {
     private static final Logger log = getLogger(MealsServlet.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("received /meals GET request");
         List<Meal> meals = getMeals();
         List<MealTo> mealTos = filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
         request.setAttribute("meals", mealTos);
